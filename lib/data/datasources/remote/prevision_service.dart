0b41130f7bce7769bd95927af41414bc589a47f3
alejandrolotito <ale@supready.app>
2026-05-23 20:15:29 +0000

    fix: correct import path for prevision_model.dart (../../models/ not ../models/)

--- lib/data/datasources/remote/prevision_service.dart
+++ lib/data/datasources/remote/prevision_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/prevision_model.dart';
+import '../../models/prevision_model.dart';
 
 // ============================================================
 // SUPReady - Servicio de Previsión Horaria
