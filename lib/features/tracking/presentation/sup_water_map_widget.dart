acaf20777a44017fb793428c0bfcbd4ffd1f5503
alejandrolotito <alejandrolotito@gmail.com>
2026-06-08 09:55:16 -0300

    Migración a Riverpod, estructura Feature-First y fix de build de Androidv2

--- lib/features/tracking/presentation/sup_water_map_widget.dart
+++ lib/features/tracking/presentation/sup_water_map_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 import '../data/tracking_provider.dart';
 
 class SupWaterMapWidget extends ConsumerStatefulWidget {
